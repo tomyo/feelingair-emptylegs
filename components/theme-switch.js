9e6958589af1f9857e21e71da3377f34adccbcd3
tomyo <tomyhayes@gmail.com>
2024-06-20 22:40:50 -0300

    Refactor theme-switch

--- components/theme-switch.js
+++ components/theme-switch.js
@@ -4,30 +4,18 @@ const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 // React to theme changes on user's system preference
 prefersDarkScheme.onchange = (e) => (root.dataset.theme = e.matches ? "🌑" : "☀️");
 
-// Choose initial theme is chosen based on user's system preference
+// Initialize theme based on user's system preference
 if (!root.dataset.theme) root.dataset.theme = prefersDarkScheme.matches ? "🌑" : "☀️";
 
 customElements.define(
   "theme-switch",
   class extends HTMLElement {
     constructor() {
-      super().attachShadow({ mode: "open" });
-      this.shadowRoot.innerHTML = /*html*/ `
-      <button aria-label="Toggle between dark and light theme">
-        <slot></slot>
-      </button>
-      
-      <style>
-        button {
-          all: unset;
-          cursor: pointer;
-        }
-      </style>
-    `;
+      super();
     }
 
     connectedCallback() {
-      this.shadowRoot.querySelector("button").addEventListener("click", this);
+      this.addEventListener("click", this);
     }
 
     handleEvent() {
